54644f749a4958047037d5d4f4bf1099b26962c2
thinhngo2106 <thinhngo210620@gmail.com>
2024-05-29 18:46:51 +0700

    update dashboard screen aws

---
+++ src/views/DashboardScreenAWS.js
@@ -0,0 +1,18 @@
+import './css/dashboard.css';
+import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { createEmbeddingContext } from 'amazon-quicksight-embedding-sdk';
+
+function DashboardScreen() {
+
+  return (
+
+    <iframe
+        width="960"
+        height="720"
+        src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/240067346498/dashboards/8251cb16-5ce5-4f3c-bd55-5554beff7bf6?directory_alias=quynhnk">
+    </iframe>
+  );
+};
+
+export default DashboardScreen
